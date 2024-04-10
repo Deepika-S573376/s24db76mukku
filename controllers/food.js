22d70e958739559e92a7ac23b3bcf3ab6eba73f7
Deepika-S573376 <divyagnanadeepika.m24@gmail.com>
2024-04-10 15:11:46 -0500

    update

--- controllers/food.js
+++ controllers/food.js
@@ -4,17 +4,9 @@ exports.food_list = function(req, res) {
  res.send('NOT IMPLEMENTED: food list');
 };
 // for a specific food.
-exports.food_detail = async function(req, res) {
-console.log("detail" + req.params.id)
-try {
-result = await food.findById( req.params.id)
-res.send(result)
-} catch (error) {
-res.status(500)
-res.send(`{"error": document for id ${req.params.id} not found`);
-}
+exports.food_detail = function(req, res) {
+ res.send('NOT IMPLEMENTED: food detail: ' + req.params.id);
 };
-
 // Handle food create on POST.
 exports.food_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: food create POST');
@@ -24,27 +16,11 @@ exports.food_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: food delete DELETE ' + req.params.id);
 };
 // Handle food update form on PUT.
-exports.food_update_put = async function(req, res) {
-console.log(`update on id ${req.params.id} with body
-${JSON.stringify(req.body)}`)
-try {
-let toUpdate = await Costume.findById( req.params.id)
-// Do updates of properties
-if(req.body.food_type)
-toUpdate.food_type = req.body.costume_type;
-if(req.body.food_size) toUpdate.cost = req.body.food_size;
-if(req.body.food_price) toUpdate.size = req.body.food_price;
-let result = await toUpdate.save();
-console.log("Sucess " + result)
-res.send(result)
-} catch (err) {
-res.status(500)
-res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
-}
+exports.food_update_put = function(req, res) {
+ res.send('NOT IMPLEMENTED: food update PUT' + req.params.id);
 };
 
-
+// List of all food
 exports.food_list = async function(req, res) {
     try{
     thefood = await food.find();
@@ -53,29 +29,26 @@ exports.food_list = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
-    }
+    } 
    };
- 
- 
+
+   // VIEWS
+// Handle a show all view
 exports.food_view_all_Page = async function(req, res) {
     try{
-    thefoods = await food.find();
-    res.render('food', { title: 'food Search Results', results: thefoods });
+    thefood = await food.find();
+    res.render('food', { title: 'food Search Results', results: thefood });
     }
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
-    }
-    };
+    } 
+   };
 
-
-    // Handle food create on POST.
+// Handle food create on POST.
 exports.food_create_post = async function(req, res) {
     console.log(req.body)
     let document = new food();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
     document.food_type = req.body.food_type;
     document.food_size = req.body.food_size;
     document.food_price = req.body.food_price;
@@ -86,6 +59,106 @@ exports.food_create_post = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
+    } 
+   }
+
+   // specific food. lab-12 s-1
+exports.food_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await food.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   }
+   
+   //lab-12 s-2
+   exports.food_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await food.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.food_type)
+            toUpdate.food_type = req.body.food_type;
+        if (req.body.food_size) toUpdate.food_size = req.body.food_size;
+        if (req.body.food_price) toUpdate.food_price = req.body.food_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
+    }
+};
+
+// Handle food delete on DELETE.
+exports.food_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await food.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+
+// Handle a show one view with id specified by query
+exports.food_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await food.findById( req.query.id)
+    res.render('fooddetail',
+    { title: 'food Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+
+exports.food_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('foodcreate', { title: 'food Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+exports.food_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await food.findById(req.query.id)
+    res.render('foodupdate', { title: 'food Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
     };
     
+    //s9
+
+    exports.food_delete_Page = async function (req, res) {
+        console.log("Delete view for id " + req.query.id)
+        try {
+            result = await food.findById(req.query.id)
+            res.render('fooddelete', {
+                title: 'food Delete', toShow:
+                    result
+            });
+        }
+        catch (err) {
+            res.status(500)
+            res.send(`{'error': '${err}'}`);
+        }
+    };
--- routes/food.js
+++ routes/food.js
@@ -1,6 +1,23 @@
 var express = require('express');
 const food_controlers= require('../controllers/food');
 var router = express.Router();
-/* GET costumes */
-router.get('/', food_controlers.food_view_all_Page );
+/* GET food */
+
+router.get('/', food_controlers.food_view_all_Page);
+
+router.put('/', function(req, res) {
+    if(req.body.checkboxsale)toUpdate.sale = true;
+    else toUpdate.same = false;
+    })
+router.get('/', food_controlers.food_delete );
+/* GET detail food page */
+router.get('/detail', food_controlers.food_view_one_Page);
+/* GET create food page */
+router.get('/create', food_controlers.food_create_Page);
+/* GET create update page */
+router.get('/update', food_controlers.food_update_Page);
+/* GET delete food page */
+router.get('/delete', food_controlers.food_delete_Page);
+
+
 module.exports = router;
