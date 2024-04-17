95decbfd4154831c0dba12331aec75351f41623d
Deepika-S573376 <divyagnanadeepika.m24@gmail.com>
2024-04-17 16:41:46 -0500

    add
    add

--- models/food.js
+++ models/food.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const foodSchema = mongoose.Schema({
 food_type: String,
 food_size: String,
-food_price: Number
+food_price: {type:Number,min:0,max:10000}
 })
 module.exports = mongoose.model("food",
 foodSchema)
--- routes/food.js
+++ routes/food.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const food_controlers= require('../controllers/food');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    res.redirect("/login");
+  };
 /* GET food */
 
 router.get('/', food_controlers.food_view_all_Page);
@@ -13,11 +20,11 @@ router.get('/', food_controlers.food_delete );
 /* GET detail food page */
 router.get('/detail', food_controlers.food_view_one_Page);
 /* GET create food page */
-router.get('/create', food_controlers.food_create_Page);
+router.get('/create',secured,food_controlers.food_create_Page);
 /* GET create update page */
-router.get('/update', food_controlers.food_update_Page);
+router.get('/update',secured, food_controlers.food_update_Page);
 /* GET delete food page */
-router.get('/delete', food_controlers.food_delete_Page);
+router.get('/delete',secured, food_controlers.food_delete_Page);
 
 
 module.exports = router;
